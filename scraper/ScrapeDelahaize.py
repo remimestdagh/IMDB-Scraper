3fb1e30940fdcc9e4ed244dece4c1c9f3406189e
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-03-04 15:43:31 +0100

    start impl delhaize

---
+++ scraper/ScrapeDelahaize.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+from scraper.HashMap import HashMap
+from scraper.Kaas import Kaas
+
+
+for i in range(4):
+    URL1='https://www.delhaize.be/nl-be/shop/Zuivel-en-kaas/Kazen/c/v2DAICHE?pageNumber={}'.format(i)
+    resultsMany = requests.get(URL1)
+    soup = BeautifulSoup(resultsMany.content,'html.parser')
+    results = soup.find(class_='DataView ProductList padded-items js-data-view')
+    # print(results)
+    kaasNamen = []
+    kaasPrijzen = []
+    kazen = []
+
+    prijzenVanKazen = results.find_all_next('span', class_='quantity-price super-bold')
+    prijzenNaKommaVanKazen = results.find_all_next('span', class_='cents')
+    namenVanKazenHTML = results.find_all_next('p', class_='text-bold title ellipsis')
+    namenVanKazen = namenVanKazenHTML
+    categorieKazen = results.find_all_next('p',class_='ellipsis')
+    for prijsKaas in prijzenVanKazen:
+        # print(prijsKaas.text, end='\n'*2)
+        kaasPrijzen.append(prijsKaas.text.strip())
+    for naamKaas in namenVanKazen:
+        # print(naamKaas.text, end='\n'*2)
+        kaasNamen.append(naamKaas.text.strip())
+    for i in range(len(prijzenVanKazen)):
+        kazen.append(Kaas(kaasNamen.__getitem__(i), kaasPrijzen.__getitem__(i)))
+        print(kazen.__getitem__(i).name +" " +kazen.__getitem__(i).price +" "+"euro")
