265465dec4f3d59dcbcaef0578a991a9e00f1aff
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-03-07 19:32:15 +0100

    omschakeling naar imdb app

--- scraper/ScrapeIMDB.py
+++ scraper/ScrapeIMDB.py
@@ -1,11 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
+from scraper.domein.Film import Film
 
-for i in range(2):
+films=[]
+teller = 0
+for i in range(5):
     url = "https://www.imdb.com/search/title/?sort=num_votes,desc&start=1&title_type=feature&year=1950,2017".format(i)
     r = requests.get(url) # where url is the above url
     bs = BeautifulSoup(r.content, 'html.parser')
+
     for movie in bs.findAll('div',class_='lister-item mode-advanced'):
         title = movie.find('img','loadlate')['alt']
         titleImage = movie.find('img', 'loadlate')['src']
@@ -15,6 +20,19 @@ for i in range(2):
         year = movie.find('span','lister-item-year text-muted unbold').contents[0]
         imdbID = movie.find('span','rating-cancel').a['href'].split('/')[2]
         stars=[]
+
         for actor in movie.find('p','').findAll('a'):
             stars.append(actor.text)
-        print(title, genres,runtime, rating, year, imdbID,stars)
+        teller=teller+1
+        print(teller,title, genres,runtime, rating, year, imdbID,stars)
+        films.append(Film(title,rating,stars))
+
+
+
+with open('films.json', 'w') as f:  # writing JSON object
+    jasonz=[]
+    for film in films:
+        jasonz.append(json.dumps(film.__dict__))
+
+    json.dump(jasonz, f)
+    print(len(jasonz))
---
+++ scraper/domein/Film.py
@@ -0,0 +1,6 @@
+class Film(object):
+    # Initializer / Instance Attributes
+    def __init__(self, titel, score, acteurs):
+        self.titel=titel
+        self.score=score
+        self.acteurs=acteurs
